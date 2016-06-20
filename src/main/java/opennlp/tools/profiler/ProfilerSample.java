1601e6d42b16d1e31aae350b9b38bcbe18b02795
beylerian <beylerian@tikemon.com>
2016-06-20 18:38:54 +0900

    updated Sample and SampleStream

--- src/main/java/opennlp/tools/profiler/ProfilerSample.java
+++ src/main/java/opennlp/tools/profiler/ProfilerSample.java
@@ -17,8 +17,78 @@
 
 package opennlp.tools.profiler;
 
+import opennlp.tools.tokenize.WhitespaceTokenizer;
+
+import java.util.*;
+
 /**
  * Created by anthony on 4/20/16.
  */
 public class ProfilerSample {
+
+  private final String age;
+  private final String gender;
+  private final List<String> text;
+
+  public ProfilerSample(String gender, String age, String text) {
+    this(gender, age, WhitespaceTokenizer.INSTANCE.tokenize(text));
+  }
+
+  public ProfilerSample(String gender, String age, String text[]) {
+    if (age == null || gender == null) {
+      throw new IllegalArgumentException("age and gender must not be null");
+    }
+    if (text == null) {
+      throw new IllegalArgumentException("text must not be null");
+    }
+
+    this.age = age;
+    this.gender = gender;
+    this.text = Collections
+      .unmodifiableList(new ArrayList<String>(Arrays.asList(text)));
+
+  }
+
+  public String getAge() {
+    return age;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public String[] getText() {
+    return text.toArray(new String[text.size()]);
+  }
+
+  @Override public String toString() {
+
+    StringBuilder sampleString = new StringBuilder();
+
+    sampleString.append(getGender() + " " + getAge()).append('\t');
+
+    for (String s : text) {
+      sampleString.append(s).append(' ');
+    }
+
+    if (sampleString.length() > 0) {
+      // remove last space
+      sampleString.setLength(sampleString.length() - 1);
+    }
+
+    return sampleString.toString();
+  }
+
+  @Override public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    } else if (obj instanceof ProfilerSample) {
+      ProfilerSample a = (ProfilerSample) obj;
+
+      return getAge().equals(a.getAge()) && getGender().equals(a.getGender())
+        && Arrays.equals(getText(), a.getText());
+    } else {
+      return false;
+    }
+  }
 }
--- src/main/java/opennlp/tools/profiler/ProfilerSampleStream.java
+++ src/main/java/opennlp/tools/profiler/ProfilerSampleStream.java
@@ -17,5 +17,47 @@
 
 package opennlp.tools.profiler;
 
-public class ProfilerSampleStream {
+import opennlp.tools.tokenize.WhitespaceTokenizer;
+import opennlp.tools.util.FilterObjectStream;
+import opennlp.tools.util.ObjectStream;
+
+import java.io.IOException;
+
+/**
+ * Created by anthony on 4/20/16.
+ */
+public class ProfilerSampleStream
+  extends FilterObjectStream<String, ProfilerSample> {
+
+  public ProfilerSampleStream(ObjectStream<String> samples) {
+    super(samples);
+  }
+
+  public ProfilerSample read() throws IOException {
+    String sampleString = samples.read();
+
+    if (sampleString != null) {
+
+      // Whitespace tokenize entire string
+      String tokens[] = WhitespaceTokenizer.INSTANCE.tokenize(sampleString);
+
+      ProfilerSample sample;
+
+      if (tokens.length > 1) {
+        String gender = tokens[0];
+        String age = tokens[1];
+        String docTokens[] = new String[tokens.length - 2];
+        System.arraycopy(tokens, 2, docTokens, 0, tokens.length - 2);
+
+        sample = new ProfilerSample(gender, age, docTokens);
+      } else {
+        throw new IOException(
+          "Empty lines, or lines with only age and gender strings are not allowed!");
+      }
+
+      return sample;
+    } else {
+      return null;
+    }
+  }
 }
